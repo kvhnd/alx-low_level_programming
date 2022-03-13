1dbd875d9cc2cd23c69d7f695f3bf5b28f275126
kvhnd <kahindinjoroge@gmail.com>
2022-03-13 16:59:08 -0700

    Prints a statement followed by a new line

--- 4-puts.c
+++ 4-puts.c
@@ -3,10 +3,11 @@
 /**
  * main-Entry point
  *
- * Return:Always 0 (Success)
+ * return:Always 0 (Success)
  */
-
 int main(void)
+/*main is the entry point*/
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 

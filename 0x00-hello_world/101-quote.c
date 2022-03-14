112abcd37b5b9feeac9078f9b9cbc3aa92fc3fbf
kvhnd <kahindinjoroge@gmail.com>
2022-03-14 16:17:33 -0700

    Prints quote followed by a newline to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints quote followed by a new line to the standard
+ * error
+ *
+ * Return: 1
+ **/
+int main(void)
+{
+	fprintf(stderr,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}

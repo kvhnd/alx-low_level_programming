ceae78e2cf2411bff0d8d8d231da1e31131e80f9
kvhnd <kahindinjoroge@gmail.com>
2022-03-16 16:39:29 -0700

    Prints _putchar followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: Prints _putchar followed by a new line
+ *
+ * Return : 0
+ *
+ **/
+int main(void)
+{
+	char text{9} = "_putchar"
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+
+	_putchar("\n");
+
+	return (0);
+}

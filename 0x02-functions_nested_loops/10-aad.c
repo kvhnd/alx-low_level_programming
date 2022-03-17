29581a462de10a2c41216782fec1ad7f4cf82d42
kvhnd <kahindinjoroge@gmail.com>
2022-03-16 19:24:30 -0700

    Adss two integers

---
+++ 0x02-functions_nested_loops/10-aad.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds integers
+ *
+ * @x: integer input
+ * @y: integer input
+ * Description: Adds two integers and returns a result
+ *
+ * Return: sum of integers
+ **/
+int add(int x, int y)
+{
+
+
+	return (x + y);
+}

9c333ea68804f7a544e33fb425612ff983c0189f
kvhnd <kahindinjoroge@gmail.com>
2022-03-18 17:03:57 -0700

    Prints the numbers from 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,15 +5,17 @@
  *
  * Description: Prints numbers between 0-9 followed by a newline
  *
+ * Retrun: 0 (success)
  **/
 void print_numbers(void)
 {
 	int i = '0';
 
-	for (;i <= '9';i++)
+	while (i <= '9')
 	{
 		_putchar(i);
 		_putchar('\n');
+		i++;
 	}
 
 

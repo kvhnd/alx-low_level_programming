f5696b9339da54da0500eda2435704ba32fcf0f9
kvhnd <kahindinjoroge@gmail.com>
2022-03-28 16:33:21 -0700

    Prints a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,12 +11,15 @@ void print_rev(char *s)
 	int c = 0;
 
 	while
-	(s[c] != '\0');
-	c++;
-
-	for (c --; c >= 0; c--)
+	(c >= 0)
 	{
-		_putchar(s[c]);
+		if (s[c] == '\0')
+			break;
+		c++;
+	}
+	for (c--; c >= 0; c--)
+	{
+	_putchar(s[c]);
 	}
 	_putchar('\n');
 }

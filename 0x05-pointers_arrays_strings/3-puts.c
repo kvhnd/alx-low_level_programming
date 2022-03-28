45c5947ad8ee94c8cf2f8e73563139f37e1155c9
kvhnd <kahindinjoroge@gmail.com>
2022-03-28 15:40:37 -0700

    Prints a string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _puts - Prints a string
+* @str: string
+*
+* Description: Prints a string followed by a newline to stdout
+*
+* Return: nothing
+**/
+void _puts(char *str)
+{
+	while
+	(*str != '\0');
+	_putchar(*str);
+	str++;
+	_putchar('\n');
+}
